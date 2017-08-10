adc8b4b79d2586af60e3c4160f7ff7f2860b6c8d
HGZprogramowanie <HGZ.programowanie@gmail.com>
2017-08-10 16:40:47 +0200

    new events od .board elements

--- dist/out.js
+++ dist/out.js
@@ -72,23 +72,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const nav = $('.nav');
   const hamburger = $('.hamburger');
+  const project = $('.project');
+  const plus_link = $('.plus_link');
+
 
   // Event: Scroll to links
 
   $(document).on("click", ".nav a", function (event) {
     event.preventDefault();
-
     $('html, body').animate({
       scrollTop: $('.' + $(this).attr('href')).offset().top
     }, 1000);
-
   });
 
-  // Functions: add/remove classes of navbar sections
+  // Functions: add/remove class hidden of navbar sections
 
   $(document).on("click", ".hamburger a", function (event) {
-
+    event.preventDefault();
     nav.toggleClass('hidden');
+  });
+
+  // Functions: add/remove class hidden of board elements
+
+  $(document).on("mouseover", ".project", function (event) {
+    event.preventDefault();
+    $(this).find(".board").removeClass('hidden');
+  });
+
+  $(document).on("mouseleave", ".project", function (event) {
+    event.preventDefault();
+    $(this).find(".board").addClass('hidden').find('.link').addClass('hidden').parent().find('.plus_link').removeClass('hidden');
+  });
+
+  $(document).on("click", ".plus_link a", function (event) {
+    event.preventDefault();
+    $(this).parent().addClass('hidden').parent().find('.link').not('.plus_link').removeClass('hidden');
   });
 
 })
