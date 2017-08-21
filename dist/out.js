afbc0b080aeb574813c38a91e87bd2dc0c5343e8
HGZprogramowanie <HGZ.programowanie@gmail.com>
2017-08-21 22:42:23 +0200

    js minified by babel

--- dist/out.js
+++ dist/out.js
@@ -67,52 +67,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-$(document).ready(() => {
-  console.log("JS ready...");
-
-  const navbarLink = $('.nav a');
-  const hamburgerLink = $('.hamburger a');
-  const project = $('.project');
-  const plusLink = $('.plus-link a');
-
-  // Event: Scroll to links
-
-  navbarLink.on("click", function (e) {
-    e.preventDefault();
-    navbarLink.parent().addClass('hidden')
-    setTimeout(() => {
-      $('html, body').animate({
-        scrollTop: $('.' + $(this).attr('href')).offset().top - 25
-      }, 1000);
-    }, 1);
-  });
-
-  // Functions: add/remove class hidden of navbar sections
-
-  hamburgerLink.on("click", function (e) {
-    e.preventDefault();
-    navbarLink.parent().toggleClass('hidden');
-  });
-
-  // Functions: add/remove class hidden of board elements
-
-  project.on("mouseover", function (e) {
-    e.preventDefault();
-    $(this).find(".board").removeClass('hidden');
-  });
-
-  project.on("mouseleave", function (e) {
-    e.preventDefault();
-    $(this).find(".board").addClass('hidden').find('.link').addClass('hidden').parent().find('.plus-link').removeClass('hidden');
-  });
-
-  plusLink.on("click", function (e) {
-    e.preventDefault();
-    $(this).parent().addClass('hidden').parent().find('.link').not('.plus-link').removeClass('hidden');
-  });
-
-})
-
+$(document).ready(()=>{console.log('JS ready...');const a=$('.nav a'),b=$('.hamburger a'),c=$('.project'),d=$('.plus-link a');a.on('click',function(b){b.preventDefault(),a.parent().addClass('hidden'),setTimeout(()=>{$('html, body').animate({scrollTop:$('.'+$(this).attr('href')).offset().top-25},1e3)},1)}),b.on('click',function(b){b.preventDefault(),a.parent().toggleClass('hidden')}),c.on('mouseover',function(a){a.preventDefault(),$(this).find('.board').removeClass('hidden')}),c.on('mouseleave',function(a){a.preventDefault(),$(this).find('.board').addClass('hidden').find('.link').addClass('hidden').parent().find('.plus-link').removeClass('hidden')}),d.on('click',function(a){a.preventDefault(),$(this).parent().addClass('hidden').parent().find('.link').not('.plus-link').removeClass('hidden')})});
 
 /***/ })
 /******/ ]);
