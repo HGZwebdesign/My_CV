dbd857e08faa0b73cafe005485dfb7f8fb31dd7b
HGZprogramowanie <HGZ.programowanie@gmail.com>
2017-08-25 18:56:28 +0200

    skewed lines

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # My CV
 
-**Link:** https://hgzwebdesign.github.io/myCV
+**Link:** https://hgzwebdesign.github.io/My_CV
 
-The introduction website containing my CV and portfolio.
+My introduction website containing CV and portfolio.
 
 ## Technologies used in this project:
 - HTML5
 - CSS3 & SASS
 - JavaScript (ES6)
-- jQuery
 - Gulp
 - Webpack
--- src/main.js
+++ src/main.js
@@ -1,45 +1,25 @@
-$(document).ready(() => {
+document.addEventListener("DOMContentLoaded", () => {
   console.log("JS ready...");
 
-  const navbarLink = $('.nav a');
-  const hamburgerLink = $('.hamburger a');
-  const project = $('.project');
-  const plusLink = $('.plus-link a');
+  const nav = $('.nav');
+  const hamburger = $('.hamburger');
 
   // Event: Scroll to links
 
-  navbarLink.on("click", function (e) {
-    e.preventDefault();
-    navbarLink.parent().addClass('hidden')
-    setTimeout(() => {
-      $('html, body').animate({
-        scrollTop: $('.' + $(this).attr('href')).offset().top - 25
-      }, 1000);
-    }, 1);
+  $(document).on("click", ".nav a", function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $('.' + $(this).attr('href')).offset().top
+    }, 1000);
+
   });
 
-  // Functions: add/remove class hidden of navbar sections
+  // Functions: add/remove classes of navbar sections
 
-  hamburgerLink.on("click", function (e) {
-    e.preventDefault();
-    navbarLink.parent().toggleClass('hidden');
-  });
+  $(document).on("click", ".hamburger a", function (event) {
 
-  // Functions: add/remove class hidden of board elements
-
-  project.on("mouseover", function (e) {
-    e.preventDefault();
-    $(this).find(".board").removeClass('hidden');
-  });
-
-  project.on("mouseleave", function (e) {
-    e.preventDefault();
-    $(this).find(".board").addClass('hidden').find('.link').addClass('hidden').parent().find('.plus-link').removeClass('hidden');
-  });
-
-  plusLink.on("click", function (e) {
-    e.preventDefault();
-    $(this).parent().addClass('hidden').parent().find('.link').not('.plus-link').removeClass('hidden');
+    nav.toggleClass('hidden');
   });
 
 })
