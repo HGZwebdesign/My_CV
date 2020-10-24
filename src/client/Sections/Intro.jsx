54d69b06fce6d221080dd99a0fc251a2d9eb6fe1
Hanna <HGZwebdesign@gmail.com>
2020-10-24 23:23:47 +0200

    Intro: lazy load + images compression

--- src/client/Sections/Intro.jsx
+++ src/client/Sections/Intro.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Box, NarrowContentBox} from 'Components/Box'
 import styled from 'styled-components'
 import bgImage from 'assets/images/bg.jpg'
+import bgImageMin from 'assets/images/bg.min.jpg'
 import {ANTI, MAIN, BLUE5, VIOLET2} from '../styles'
 import {L, M, Text, XL_BOLD, XXL_BOLD} from '../Components/Text'
+import portraitMin from 'assets/images/portrait.min.png'
 import portrait from 'assets/images/portrait.png'
 import {SectionBox} from './MainPanel'
 import {useMediaQuery} from 'plugins/MediaQuery'
 
 const SectionBoxBg = styled(SectionBox)`
-	background-image: url(${bgImage});
+	background-image: url(${p => p.img});
 	background-size: cover;
 	background-position: center;
 	background-repeat: no-repeat;
 `
 
 const ImgBox = styled(Box)`
-	background-image: url(${portrait});
+	background-image: url(${p => p.img});
 	background-size: cover;
 	background-position: center;
 	background-repeat: no-repeat;
@@ -32,10 +34,33 @@ const TextBox = styled(Box)`
 	z-index: 1;
 `
 
+const useProgressiveImage = src => {
+	const [sourceLoaded, setSourceLoaded] = useState()
+
+	useEffect(() => {
+		const img = new Image()
+		img.src = src
+		img.addEventListener('load', () => setSourceLoaded(src))
+	}, [src])
+
+	return sourceLoaded
+}
+
 const Intro = ({id}) => {
+	const loadedImg = useProgressiveImage(portrait)
+	const loadedImg2 = useProgressiveImage(bgImage)
 	const {isPhone} = useMediaQuery()
+
 	return (
-		<SectionBoxBg {...{bg: MAIN, fg: ANTI, id, minHeight: '100vh'}}>
+		<SectionBoxBg
+			{...{
+				bg: MAIN,
+				fg: ANTI,
+				id,
+				minHeight: '100vh',
+				img: loadedImg2 || bgImageMin,
+			}}
+		>
 			<NarrowContentBox relative bottom right>
 				<TextBox>
 					<Box column gap left>
@@ -58,7 +83,7 @@ const Intro = ({id}) => {
 						</Box>
 					</Box>
 				</TextBox>
-				<ImgBox size={70} />
+				<ImgBox size={70} img={loadedImg || portraitMin} />
 			</NarrowContentBox>
 		</SectionBoxBg>
 	)
