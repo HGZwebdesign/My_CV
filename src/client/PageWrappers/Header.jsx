2340d720266dd3ae748b7d5dd6831b832ff868b1
Hanna <HGZwebdesign@gmail.com>
2020-10-24 20:52:45 +0200

    Header: shrink menu on scroll

--- src/client/PageWrappers/Header.jsx
+++ src/client/PageWrappers/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Box, ContentBox} from 'Components/Box'
 import {MAIN, ANTI, Theme, headerTextTheme} from 'Styles'
 import {useMediaQuery} from 'plugins/MediaQuery'
@@ -14,13 +14,23 @@ const WrapBox = styled(Box)`
 
 const Header = () => {
 	const {isPhone} = useMediaQuery()
+	const [headerWidth, setHeaderWidth] = useState(1.5)
+
+	const listenScrollEvent = () => {
+		// eslint-disable-next-line no-unused-expressions
+		window.scrollY > 100 ? setHeaderWidth(0.5) : setHeaderWidth(1.5)
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', listenScrollEvent)
+	})
 
 	return (
 		<Theme theme={headerTextTheme}>
 			<WrapBox {...{bg: MAIN, fg: ANTI, shadow: 2}}>
 				<ContentBox
 					{...{
-						padding: isPhone ? '0.5rem' : '1rem',
+						padding: isPhone ? '0.5rem' : `${headerWidth}rem 1rem`,
 						top: true,
 						relative: true,
 					}}
