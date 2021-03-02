4e93b611d1e1a337ced4324359a880f8158fa957
Hanna <HGZwebdesign@gmail.com>
2021-03-02 15:43:15 +0100

    randomRange

--- src/client/helpers.js
+++ src/client/helpers.js
@@ -5,3 +5,5 @@ export const makeDate = dateArr =>
 	[dateArr[0], dateArr[1] < 10 ? ('0' + dateArr[1]).slice(-2) : dateArr[1]]
 		.reverse()
 		.join('.')
+
+export const randomRange = (min, max) => Math.floor(Math.random() * max) + min
