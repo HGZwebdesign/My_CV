7533350c394522bc4e60c3fff3fecbda5b379a4d
Hanna <HGZwebdesign@gmail.com>
2021-03-06 16:48:39 +0100

    CV: preview hidden for mobile

--- src/client/Pages/CV/index.jsx
+++ src/client/Pages/CV/index.jsx
@@ -7,7 +7,21 @@ import LeftBar from './LeftBar'
 import RightBar from './RightBar'
 import {BlueButton, VioletButton} from 'src/client/Components/Buttons'
 import Link from 'src/client/Components/Link'
-import {M} from 'src/client/Components/Text'
+import {M, Text} from 'src/client/Components/Text'
+import {useMediaQuery} from 'plugins/MediaQuery'
+import styled from 'styled-components'
+import ImageBox from 'src/client/Components/ImageBox'
+import cvPreview from 'assets/images/cv-preview.png'
+
+const ImgBox = styled(ImageBox)`
+	width: 87%;
+	padding-top: 127%;
+	filter: blur(3px);
+`
+
+const WrapBox = styled(Box)`
+	display: ${p => p.hidden && 'none'};
+`
 
 const CVPanel = ({cvRef}) => {
 	return (
@@ -38,17 +52,30 @@ const Buttons = ({handlePrint}) => {
 }
 
 const CV = () => {
+	const {isPhone} = useMediaQuery()
 	const cvRef = useRef()
 	const handlePrint = useReactToPrint({
 		content: () => cvRef.current,
 		documentTitle: `${LAST_NAME} ${FIRST_NAME} - CV`,
 	})
 	return (
-		<Box {...{bg: CV_LIGHT, top: true, padding: '0 0 1rem'}}>
+		<Box
+			{...{bg: CV_LIGHT, top: true, padding: '0 0 1rem', minHeight: '100vh'}}
+		>
 			<Theme theme={CVTextTheme}>
 				<Box top column {...{width: '21cm'}}>
 					<Buttons {...{handlePrint}} />
-					<CVPanel {...{cvRef}} />
+					<WrapBox {...{hidden: isPhone}}>
+						<CVPanel {...{cvRef}} />
+					</WrapBox>
+					{isPhone && (
+						<Box {...{padding: '0 1rem 1rem'}} column gap>
+							<Text sets={[M]}>
+								Sorry, preview is available for larger screens only.
+							</Text>
+							<ImgBox {...{src: cvPreview}} />
+						</Box>
+					)}
 				</Box>
 			</Theme>
 		</Box>
