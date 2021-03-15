16922912409b085be3f24b1fd1d2f788049377c6
Hanna <HGZwebdesign@gmail.com>
2021-03-15 22:04:08 +0100

    Icon: glyphs

--- src/client/Components/Icon.jsx
+++ src/client/Components/Icon.jsx
@@ -1,27 +1,27 @@
 import {Box} from 'Components/Box'
 import {styled} from 'Styles'
 
-export const icons = {
-	email: require('assets/icons/icon-email.svg'),
-	phone: require('assets/icons/icon-phone.svg'),
-	linkedin: require('assets/icons/icon-linkedin.svg'),
+export const glyphs = {
+	email: require('assets/icons/email.svg'),
+	phone: require('assets/icons/phone.svg'),
+	linkedin: require('assets/icons/linkedin.svg'),
+	github: require('assets/icons/github.svg'),
 }
 
-const IconBox = styled(Box).attrs({cursor: true})`
-	width: ${(p) => p.width || p.sizeBg};
-	height: ${(p) => p.height || p.sizeBg};
+export const IconBox = styled(Box).attrs({cursor: true})`
+	width: ${p => p.width || p.sizeBg};
+	height: ${p => p.height || p.sizeBg};
 
-	border-radius: ${(p) => p.round && `50%`};
+	border-radius: ${p => p.round && `50%`};
 
-	background-image: url(${(p) => p.icon});
+	background-image: url(${p => p.icon});
 	background-repeat: no-repeat;
 	background-position: center;
-	background-size: ${(p) => p.size};
+	background-size: ${p => p.size};
 
 	transition: 0.5s;
-	${(p) => p.theme.colors}
+	${p => p.theme.colors}
 `
 
 // const Icon = ({Svg, ...p}) => <IconBase {...p}>{/* <Svg /> */}</IconBase>
 
-export default IconBox
